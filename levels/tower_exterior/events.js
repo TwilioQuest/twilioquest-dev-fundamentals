ef7a0f5969cfabc2e61d9e2f58ff323ff5384ead
Ryan Kubik <rkubik@twilio.com>
2022-06-10 14:20:54 -0700

    Tweak more of map and animation

--- levels/tower_exterior/events.js
+++ levels/tower_exterior/events.js
@@ -1,5 +1,5 @@
-// viewpoint_tower
 const merge = require("lodash.merge");
+const viewTower = require("./events/viewTower");
 
 const WORLD_STATE_KEY = "com.twilioquest.developer-fundamentals";
 
@@ -9,36 +9,7 @@ module.exports = function (event, world) {
   const worldState = merge(INITIAL_STATE, world.getState(WORLD_STATE_KEY));
 
   if (event.name === "playerDidInteract" && event.target.key === "telescope") {
-    world.forEachEntities("viewpoint_tower_door", async (viewpoint) => {
-      world.disablePlayerMovement();
-
-      await world.tweenCameraToPosition({
-        x: viewpoint.startX,
-        y: viewpoint.startY,
-      });
-      world.forEachEntities("viewpoint_tower", async (viewpoint) => {
-        await world.tweenCameraToPosition(
-          {
-            x: viewpoint.startX,
-            y: viewpoint.startY,
-          },
-          { duration: 5000 }
-        );
-        await world.wait(3000);
-        world.forEachEntities("viewpoint_tower_door", async (viewpoint) => {
-          await world.tweenCameraToPosition(
-            {
-              x: viewpoint.startX,
-              y: viewpoint.startY,
-            },
-            { duration: 5000 }
-          );
-          await world.tweenCameraToPlayer();
-
-          world.enablePlayerMovement();
-        });
-      });
-    });
+    viewTower(world);
   }
 
   world.setState(WORLD_STATE_KEY, worldState);
