4319f9554fdf8c9fba68fab2af852dcff8a0e15f
Ryan Kubik <rkubik@twilio.com>
2022-06-10 17:42:10 -0700

    Create landing map and initial tower tween

--- levels/tower_exterior/events.js
+++ levels/tower_exterior/events.js
@@ -3,12 +3,24 @@ const viewTower = require("./events/viewTower");
 
 const WORLD_STATE_KEY = "com.twilioquest.developer-fundamentals";
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  hasStartedInitialTowerTween: false,
+};
 
 module.exports = function (event, world) {
   const worldState = merge(INITIAL_STATE, world.getState(WORLD_STATE_KEY));
 
   if (event.name === "playerDidInteract" && event.target.key === "telescope") {
+    viewTower(world);
+  }
+
+  if (
+    event.name === "triggerAreaWasEntered" &&
+    event.target.key === "triggerViewTower" &&
+    !worldState.hasStartedInitialTowerTween
+  ) {
+    worldState.hasStartedInitialTowerTween = true;
+
     viewTower(world);
   }
 
