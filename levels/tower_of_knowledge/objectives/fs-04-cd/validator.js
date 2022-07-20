c42717e990d3e3f8f5514bcadda0e04d3e076907
Ryan Kubik <rkubik@twilio.com>
2022-07-20 16:30:47 -0700

    Implement validator 4

--- levels/tower_of_knowledge/objectives/fs-04-cd/validator.js
+++ levels/tower_of_knowledge/objectives/fs-04-cd/validator.js
@@ -1,39 +1,51 @@
-const assert = require("assert");
-
-const assertTestCase = (testFunction) => (input, expected) => {
-  const testResult = testFunction(input);
-
-  assert.strictEqual(
-    testResult,
-    expected,
-    `Expected "${expected}" from input "${input}", but received "${testResult}".`
-  );
-};
-
 module.exports = async function (helper) {
-  let context;
-
   try {
-    context = await helper.pullVarsFromQuestIdeUserCodeLocalScope(
-      ["differenceMinMax"],
-      "difference-max-min"
-    );
+    const pwd1 = helper.getNormalizedInput("pwd1", {
+      lowerCase: false,
+    });
+    const pwd2 = helper.getNormalizedInput("pwd2", {
+      lowerCase: false,
+    });
 
-    assert(
-      context.differenceMinMax,
-      "The function differenceMinMax is not defined!"
-    );
+    if (!pwd1) {
+      helper.fail(
+        "You need to provide a file path for the <em>first</em> present working directory in the Hack Interface!"
+      );
+      return;
+    }
 
-    const test = assertTestCase(context.differenceMinMax);
+    if (!pwd2) {
+      helper.fail(
+        "You need to provide a file path for the <em>second</em> present working directory in the Hack Interface!"
+      );
+      return;
+    }
 
-    test([1, 2, 3, 4, 5], 4);
-    test([100, 0], 100);
-    test([3.3, 5, -2, 5], 7);
-    test([8, 1.2, 5, 9], 7.8);
+    const pathParts1 = helper.getFilePathPartsArray(pwd1);
+    const expectedPathParts1 = ["cedric", "robot_thoughts"];
+
+    if (!helper.areArrayContentsEqual(pathParts1, expectedPathParts1)) {
+      helper.fail(
+        `Your entered file path "${pwd1}" did not match the expected path after changing to the "robot_thoughts" directory.`
+      );
+      return;
+    }
+
+    const pathParts2 = helper.getFilePathPartsArray(pwd2);
+    const expectedPathParts2 = ["cedric"];
+
+    if (!helper.areArrayContentsEqual(pathParts2, expectedPathParts2)) {
+      helper.fail(
+        `Your entered file path "${pwd2}" did not match the expected path after trying to change to the "fog_owl_computations.csv" file.`
+      );
+      return;
+    }
   } catch (err) {
     helper.fail(err);
     return;
   }
 
-  helper.success("You did it!");
+  helper.success(`
+  You found Cedric's YouTube video! He'll be delighted to hear about this!
+`);
 };
